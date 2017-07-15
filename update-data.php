820cb4c61973e6fcb7d4bf422309937fdc2dfa78
aditx <klikadsene@gmail.com>
2017-07-15 07:00:02 +0700

    update data 6

--- update-data.php
+++ update-data.php
@@ -7,20 +7,20 @@
 	$con = mysql_connect($host,$uname,$pwd) or die("connection failed");
 	mysql_select_db($db,$con) or die("db selection failed");
 
-  $idTBL_T_SEMINAR = $_REQUEST['idTBL_T_SEMINAR'];
-  $idTBL_M_SEMINAR = $_REQUEST['idTBL_M_SEMINAR'];
-  $NAMA = $_REQUEST['NAMA'];
-  $ALAMAT = $_REQUEST['ALAMAT'];
-  $JENIS_KELAMIN = $_REQUEST['JENIS_KELAMIN'];
-  $KTP = $_REQUEST['KTP'];
-  $EMAIL = $_REQUEST['EMAIL'];
-  $NO_TELPON = $_REQUEST['NO_TELPON'];
-  $TEMPAT_LAHIR = $_REQUEST['TEMPAT_LAHIR'];
+  $idTBL_T_SEMINAR = $_REQUEST['idTBL_T_SEMINARs'];
+  $idTBL_M_SEMINAR = $_REQUEST['idTBL_M_SEMINARs'];
+  $NAMA = $_REQUEST['NAMAs'];
+  $ALAMAT = $_REQUEST['ALAMATs'];
+  $JENIS_KELAMIN = $_REQUEST['JENIS_KELAMINs'];
+  $KTP = $_REQUEST['KTPs'];
+  $EMAIL = $_REQUEST['EMAILs'];
+  $NO_TELPON = $_REQUEST['NO_TELPONs'];
+  $TEMPAT_LAHIR = $_REQUEST['TEMPAT_LAHIRs'];
 
   $result = mysql_query("UPDATE TBL_T_SEMINAR SET idTBL_M_SEMINAR = '$idTBL_M_SEMINAR', NAMA = '$NAMA', ALAMAT = '$ALAMAT', KTP = '$KTP', EMAIL = '$EMAIL', NO_TELPON = '$NO_TELPON', TEMPAT_LAHIR = '$TEMPAT_LAHIR' WHERE idTBL_T_SEMINAR = '$idTBL_T_SEMINAR'", $con);
 	if($result)
   {
-		$flag['code']= $NAMA;
+		$flag['code']= "Update sukses";
     print(json_encode($flag));
 	} else {
     $flag['code']= "Update gagal";
